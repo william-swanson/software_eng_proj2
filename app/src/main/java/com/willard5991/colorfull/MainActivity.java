0336869bdf4809c0a2c4959ba5c8b07d3b0a9423
william-swanson <31480197+william-swanson@users.noreply.github.com>
2017-11-20 09:55:40 -0600

    Revert "Swanny1"

--- app/src/main/java/com/willard5991/colorfull/MainActivity.java
+++ app/src/main/java/com/willard5991/colorfull/MainActivity.java
@@ -1,10 +1,7 @@
 package com.willard5991.colorfull;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,16 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = (Button) findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(),ChooseActivity.class);
-                startActivity(intent);
-            }
-        });
-
     }
 }
