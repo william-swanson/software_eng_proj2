89c8f6921808e991aea7745ae52b3fa66cdb03dd
MitchellFabian <mitchell.fabian@drake.edu>
2017-11-28 14:50:45 -0600

    Calendar

--- app/src/main/java/com/willard5991/colorfull/CalendarFragment.java
+++ app/src/main/java/com/willard5991/colorfull/CalendarFragment.java
@@ -2,17 +2,20 @@ package com.willard5991.colorfull;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CalendarFragment extends Fragment {
 
+    private CalendarView calendar;
 
     public CalendarFragment() {
         // Required empty public constructor
@@ -22,8 +25,17 @@ public class CalendarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        calendar = (CalendarView) view.findViewById(R.id.calendar_view);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
+                Toast.makeText(getActivity(),dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
     }
 
 }
